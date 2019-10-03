ea6f0c2553853fc6c101c06fa4fc2ec52852f6d1
gleeyan <25153775+gleeyan@users.noreply.github.com>
2019-10-03 17:14:11 -0500

    Add files via upload

---
+++ AdminDash/src/main/java/com/codingdojo/admindash/services/RoleService.java
@@ -0,0 +1,20 @@
+package com.codingdojo.admindash.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.admindash.models.Role;
+import com.codingdojo.admindash.repositories.RoleRepository;
+
+@Service
+public class RoleService {
+	private RoleRepository roleRepository;
+	
+	public RoleService(RoleRepository roleRepository)     {
+        this.roleRepository = roleRepository;
+    }
+    public Role findByName(String name) {
+        return roleRepository.findByName(name);
+    }
+}
